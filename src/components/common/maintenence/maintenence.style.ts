fad54aa9cdeb3cea2a37306757579bc2c5fa2e93
jaysukhtarsariya <jaysukhprajapati1999@gmail.com>
2023-03-11 06:30:49 -0800

    website setup and under-maintenence

---
+++ src/components/common/maintenence/maintenence.style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyleMaintenence = styled.div`
+ display: grid;
+ place-items: center;
+ height: 100vh;
+ width: 100vw;
+`
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,17 @@
+import Header from './header'
+import Footer from './footer'
+import { ReactNode } from 'react'
+
+interface Props {
+    children?: ReactNode
+}
+
+export default function Layout({ children }: Props) {
+    return (
+        <>
+            <Header />
+              <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
